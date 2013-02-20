8c5a107f334b5f30821fe5f532431c4017fbc804
Michael Evans <mevans@metallacycle.com>
2013-02-20 07:20:48 -0600

    Revert "Revert "Revert "Revert "Consensus & profile; FASTA reader template""""
    This reverts commit ab239bb07c51b4f5e455275d1aef7036c57860b7.

--- hamming_distance.py
+++ hamming_distance.py
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 def hamming_distance(seqA, seqB):
+=======
+def hamming_distance(sequenceA, sequenceB):
+>>>>>>> Consensus & profile; FASTA reader template
 	"""Given: Two DNA strings s and t of equal length (not exceeding 1 kbp).
 	
 	Returns: The Hamming distance dH(s,t). Given two strings s and t of equal length, the
@@ -27,5 +31,11 @@ try:
 except IOError:
 	print "A file does not exist at this location, or some other I/O error occurred. Peace out!"
 	sys.exit()
+<<<<<<< HEAD
 print "Hamming distance:"
 print hamming_distance(f.readline()[:-1], f.readline()[:-1])
+=======
+
+dist = hamming_distance(f.readline()[:-1], f.readline()[:-1])
+print "\nHamming distance: " + str(dist) + "\n"
+>>>>>>> Consensus & profile; FASTA reader template
