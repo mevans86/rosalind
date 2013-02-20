04b0e51f6589e62dcb4f67c3bb525b69cd9b48f6
Michael Evans <mevans@metallacycle.com>
2013-02-20 07:20:48 -0600

    Revert "Revert "Revert "Consensus & profile; FASTA reader template"""
    This reverts commit 14d98065b419be26fcff99f335385b265c75db80.

--- translate_mrna_to_protein.py
+++ translate_mrna_to_protein.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def translate_mRNA_to_protein(seq):
 	"""Given an RNA string seq corresponding to a strand of mRNA, return the protein string encoded by seq."""
 	tDict = { "UUU":"F",
@@ -80,31 +79,6 @@ def translate_mRNA_to_protein(seq):
 		protein.append(tDict[seq[pos:endOfSlice]])
 		pos = pos + 3
 	return "".join(protein)
-=======
-def translate_mRNA_to_protein(mRNAstring):
-	"""Given: An RNA string s corresponding to a strand of mRNA (of length at most 10 kbp).
-
-	Return: The protein string encoded by s."""
-	
-	translationDictionary = { "UUU" : "F", "CUU" : "L", "AUU" : "I", "GUU" : "V", "UUC" : "F", "CUC" : "L", "AUC" : "I", "GUC" : "V", "UUA" : "L", "CUA" : "L", "AUA" : "I", "GUA" : "V", "UUG" : "L", "CUG" : "L", "AUG" : "M", "GUG" : "V", "UCU" : "S", "CCU" : "P", "ACU" : "T", "GCU" : "A", "UCC" : "S", "CCC" : "P", "ACC" : "T", "GCC" : "A", "UCA" : "S", "CCA" : "P", "ACA" : "T", "GCA" : "A", "UCG" : "S", "CCG" : "P", "ACG" : "T", "GCG" : "A", "UAU" : "Y", "CAU" : "H", "AAU" : "N", "GAU" : "D", "UAC" : "Y", "CAC" : "H", "AAC" : "N", "GAC" : "D", "UAA" : "Stop", "CAA" : "Q", "AAA" : "K", "GAA" : "E", "UAG" : "Stop", "CAG" : "Q", "AAG" : "K", "GAG" : "E", "UGU" : "C", "CGU" : "R", "AGU" : "S", "GGU" : "G", "UGC" : "C", "CGC" : "R", "AGC" : "S", "GGC" : "G", "UGA" : "Stop", "CGA" : "R", "AGA" : "R", "GGA" : "G", "UGG" : "W", "CGG" : "R", "AGG" : "R", "GGG" : "G" }
-	loopPosition = 0
-	aminoAcidList = []
-	error = 0
-	while(loopPosition < len(mRNAstring)):
-		endOfSlice = loopPosition + 3
-		if(not (mRNAstring[loopPosition:endOfSlice] in translationDictionary.keys())):
-			print("Error translating mRNA: a bad codon was discovered somewhere in your mRNA sequence!")
-			error = 1
-			break
-		if(translationDictionary[mRNAstring[loopPosition:endOfSlice]] == "Stop"):
-			break
-		aminoAcidList.append(translationDictionary[mRNAstring[loopPosition:endOfSlice]])
-		loopPosition = loopPosition + 3
-	if(error == 0):	
-		return "".join(aminoAcidList)
-	else:
-		return "Error"
->>>>>>> Consensus & profile; FASTA reader template
 # end translate_mRNA_to_protein
 
 # main block
@@ -113,11 +87,5 @@ try:
 except IOError:
 	print "A file does not exist at this location, or some other I/O error occurred. Peace out!"
 	sys.exit()
-<<<<<<< HEAD
 print "Translated protein:"
 print translate_mRNA_to_protein(f.readline()[:-1])
-=======
-
-proteinSeq = translate_mRNA_to_protein(f.readline())
-print "\nTranslated protein: " + str(proteinSeq) + "\n"
->>>>>>> Consensus & profile; FASTA reader template
